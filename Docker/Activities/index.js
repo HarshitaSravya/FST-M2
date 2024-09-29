b513a973b8f8dab2d71b8a6ce0f89b1d222a70c4
HarshitaSravya <53606424+HarshitaSravya@users.noreply.github.com>
2024-09-29 21:06:41 +0530

    Files for Complex activity
    Uploaded files for complex activity

--- Docker/Activities/index.js
+++ Docker/Activities/index.js
@@ -1,27 +1,71 @@
+const keys = require("./keys");
+
+// Express App Setup
 const express = require("express");
-const redis = require("redis");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const client = redis.createClient({
-    host: "redis-server",
-    port:6379
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
-client.get("visits", (err, visits) => {
-  if (visits === null) {
-    client.set("visits", 0);
-  } else {
-    client.set("visits", parseInt(visits));
-  }
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
 });
+
+// Redis Client Setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
 
 app.get("/", (req, res) => {
-  client.get("visits", (err, visits) => {
-    res.send("Number of visits is " + visits);
-    client.set("visits", parseInt(visits) + 1);
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
   });
 });
 
-app.listen(8081, () => {
-  console.log("Listening on port 8081");
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log("Listening");
 });
